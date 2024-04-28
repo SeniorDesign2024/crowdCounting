90fd4c9cd1e571f3387934d0af0fe47da9317f3c
smistry4 <smistry1@vols.utk.edu>
2024-04-28 13:54:33 -0400

    added model switching

--- app.py
+++ app.py
@@ -1,26 +1,33 @@
 from flask import Flask, request, jsonify
 from lwcc import LWCC
-# from flask_cors import CORS
 import base64
 import os
 import random
 import string
 
-
 app = Flask(__name__)
-# CORS(app) 
-csrnet_model_a = LWCC.load_model(model_name = "CSRNet", model_weights="SHA")
-bay_model_a = LWCC.load_model(model_name= "Bay", model_weights="SHA")
 dmcount_model_a = LWCC.load_model(model_name= "DM-Count", model_weights="SHA")
-csrnet_model_b = LWCC.load_model(model_name = "CSRNet", model_weights="SHB")
-bay_model_b = LWCC.load_model(model_name= "Bay", model_weights="SHB")
 dmcount_model_b = LWCC.load_model(model_name= "DM-Count", model_weights="SHB")
-sfanet_model = LWCC.load_model(model_name= "SFANet", model_weights="SHB")
+#csrnet_model_a = LWCC.load_model(model_name = "CSRNet", model_weights="SHA")
+#bay_model_a = LWCC.load_model(model_name= "Bay", model_weights="SHA")
+#csrnet_model_b = LWCC.load_model(model_name = "CSRNet", model_weights="SHB")
+#bay_model_b = LWCC.load_model(model_name= "Bay", model_weights="SHB")
+#sfanet_model = LWCC.load_model(model_name= "SFANet", model_weights="SHB")
 
+'''
+Dummy route
+'''
 @app.route("/")
 def home():
     return "Hello World!"
 
+'''
+Counts the number of people in an image.
+
+Also, performs image conversion from base64 and storage for the use in ML model.
+
+@return {number} number of people in an image.
+'''
 @app.route("/countingService", methods=['POST'])
 def countingService():
     if 'image' not in request.json:
@@ -28,27 +35,36 @@ def countingService():
     
     event_id = request.json['event_id']
     image_data = request.json['image']
+    modelToUse = request.json['model']
     
-    # Remove the base64 prefix if it exists
+    # Decode the image
+    image_type = ''
     if image_data.startswith('data:image/jpeg;base64,'):
         image_data = image_data.replace('data:image/jpeg;base64,', '')
-    
-    # Convert base64 to bytes
+        image_type = 'jpg'
+    elif image_data.startswith('data:image/png;base64,'):
+        image_data = image_data.replace('data:image/png;base64,', '')
+        image_type = 'png'
     image_bytes = base64.b64decode(image_data)
     
-    # Generate a event directory and a random filename
+    # Store the image
     if not os.path.exists(event_id):
         os.makedirs(event_id)
-    filename = event_id + '/' + generate_random_string(15) + '.jpg'
-    
-    # Write the image data to a file
+    filename = ''
+    if image_type == 'jpg':
+        filename = event_id + '/' + generate_random_string(15) + '.jpg'
+    elif image_type == 'png':
+        filename = event_id + '/' + generate_random_string(15) + '.png'
     with open(filename, 'wb') as f:
         f.write(image_bytes)
     
-    # Run algorithm (you would replace this with your actual image processing code)
-    count = LWCC.get_count(filename, model = dmcount_model_a)
+    # Model selection and counting
+    if modelToUse == 'dense':
+        modelToUse = dmcount_model_a
+    elif modelToUse == 'sparse':
+        modelToUse = dmcount_model_b
+    count = LWCC.get_count(filename, model = modelToUse)
 
-    # For demonstration purposes, let's just print the filename
     print(f"Image processed: {filename}")
     
     return jsonify({'message': 'success', 'count': int(count)}), 200
